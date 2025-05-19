e2c269aa3a2b0bd264391d9a6a94dc72104e1f6a
VeaNika <veronika.sonya90@gmail.com>
2025-05-18 18:48:30 -0600

    Removed unnecessary fix32 casts

--- code/core/arm9/source/GbaColorCorrection/ColorProfiles.h
+++ code/core/arm9/source/GbaColorCorrection/ColorProfiles.h
@@ -25,121 +25,121 @@ struct ColorProfile
 const ColorProfile Agb001 =
 {
     {
-        { fix32<12>(0.905f), fix32<12>(0.195f), fix32<12>(-0.1f) },
-        { fix32<12>(0.1f), fix32<12>(0.65f), fix32<12>(0.25f) },
-        { fix32<12>(0.1575f), fix32<12>(0.1425f), fix32<12>(0.7f) }
+        { 0.905f, 0.195f, -0.1f },
+        { 0.1f, 0.65f, 0.25f },
+        { 0.1575f, 0.1425f, 0.7f }
     },
-    fix32<12>(0.91f)
+    0.91f
 };
 
 // libretro GBA SP (AGS-101) color (sRGB).
 const ColorProfile Ags101 =
 {
     {
-        { fix32<12>(0.96f), fix32<12>(0.11f), fix32<12>(-0.07f) },
-        { fix32<12>(0.0325f), fix32<12>(0.89f), fix32<12>(0.0775f) },
-        { fix32<12>(0.001f), fix32<12>(-0.03f), fix32<12>(1.029f) }
+        { 0.96f, 0.11f, -0.07f },
+        { 0.0325f, 0.89f, 0.0775f },
+        { 0.001f, -0.03f, 1.029f }
     },
-    fix32<12>(0.935f)
+    0.935f
 };
 
 // libretro GB micro OXY-001 color (sRGB).
 const ColorProfile Oxy001 =
 {
     {
-        { fix32<12>(0.8025f), fix32<12>(0.31f), fix32<12>(-0.1125f) },
-        { fix32<12>(0.1f), fix32<12>(0.6875f), fix32<12>(0.2125f) },
-        { fix32<12>(0.1225f), fix32<12>(0.1125f), fix32<12>(0.765f) }
+        { 0.8025f, 0.31f, -0.1125f },
+        { 0.1f, 0.6875f, 0.2125f },
+        { 0.1225f, 0.1125f, 0.765f }
     },
-    fix32<12>(0.9f)
+    0.9f
 };
 
 //// libretro NDS Phat NTR-001 color (sRGB).
 const ColorProfile Ntr001 =
 {
     {
-        { fix32<12>(0.835f), fix32<12>(0.27f), fix32<12>(-0.105f) },
-        { fix32<12>(0.1f), fix32<12>(0.6375f), fix32<12>(0.2625f) },
-        { fix32<12>(0.105f), fix32<12>(0.175f), fix32<12>(0.72f) }
+        { 0.835f, 0.27f, -0.105f },
+        { 0.1f, 0.6375f, 0.2625f },
+        { 0.105f, 0.175f, 0.72f }
     },
-    fix32<12>(0.905f)
+    0.905f
 };
 
 // libretro NDS lite USG-001 color (sRGB).
 const ColorProfile Usg001 =
 {
     {
-        { fix32<12>(0.93f), fix32<12>(0.14f), fix32<12>(-0.07f) },
-        { fix32<12>(0.025f), fix32<12>(0.9f), fix32<12>(0.075f) },
-        { fix32<12>(0.008f), fix32<12>(-0.03f), fix32<12>(1.022f) }
+        { 0.93f, 0.14f, -0.07f },
+        { 0.025f, 0.9f, 0.075f },
+        { 0.008f, -0.03f, 1.022f }
     },
-    fix32<12>(0.935f)
+    0.935f
 };
 
 // libretro PSP 1000 color (sRGB).
 const ColorProfile Psp01g =
 {
     {
-        { fix32<12>(0.835f), fix32<12>(0.155f), fix32<12>(-0.135f) },
-        { fix32<12>(0.0425f), fix32<12>(0.745f), fix32<12>(0.155f) },
-        { fix32<12>(0.0225f), fix32<12>(0.0145f), fix32<12>(0.963f) }
+        { 0.835f, 0.155f, -0.135f },
+        { 0.0425f, 0.745f, 0.155f },
+        { 0.0225f, 0.0145f, 0.963f }
     },
-    fix32<12>(0.99f)
+    0.99f
 };
 
 // libretro Nintendo Switch GBA Classics color (sRGB).
 const ColorProfile NswIps =
 {
     {
-        { fix32<12>(0.865f), fix32<12>(0.1225f), fix32<12>(0.0125f) },
-        { fix32<12>(0.0575f), fix32<12>(0.925f), fix32<12>(0.0125f) },
-        { fix32<12>(0.0575f), fix32<12>(0.1225f), fix32<12>(0.82f) }
+        { 0.865f, 0.1225f, 0.0125f },
+        { 0.0575f, 0.925f, 0.0125f },
+        { 0.0575f, 0.1225f, 0.82f }
     },
-    fix32<12>(1.f)
+    1.f
 };
 
 // libretro Nintendo Switch GBA Classics Oled color (sRGB).
 const ColorProfile NswOle =
 {
     {
-        { fix32<12>(1.54f), fix32<12>(-0.48f), fix32<12>(-0.06f) },
-        { fix32<12>(-0.045f), fix32<12>(1.0625f), fix32<12>(-0.0175f) },
-        { fix32<12>(-0.0225f), fix32<12>(-0.075f), fix32<12>(1.1025f) }
+        { 1.54f, -0.48f, -0.06f },
+        { -0.045f, 1.0625f, -0.0175f },
+        { -0.0225f, -0.075f, 1.1025f }
     },
-    fix32<12>(1.f)
+    1.f
 };
 
 // libretro Visual Boy Advance color.
 const ColorProfile VbaEmu =
 {
     {
-        { fix32<12>(0.73f), fix32<12>(0.27f), fix32<12>(0.f) },
-        { fix32<12>(0.0825f), fix32<12>(0.6775f), fix32<12>(0.24f) },
-        { fix32<12>(0.0825f), fix32<12>(0.24f), fix32<12>(0.6775f) }
+        { 0.73f, 0.27f, 0.f },
+        { 0.0825f, 0.6775f, 0.24f },
+        { 0.0825f, 0.24f, 0.6775f }
     },
-    fix32<12>(1.f)
+    1.f
 };
 
 // libretro No$GBA full color.
 const ColorProfile NoCash =
 {
     {
-        { fix32<12>(0.62f), fix32<12>(0.1913f), fix32<12>(-0.0917f) },
-        { fix32<12>(0.004f), fix32<12>(0.56f), fix32<12>(0.167f) },
-        { fix32<12>(0.004f), fix32<12>(0.167f), fix32<12>(0.56f) },
+        { 0.62f, 0.1913f, -0.0917f },
+        { 0.004f, 0.56f, 0.167f },
+        { 0.004f, 0.167f, 0.56f },
     },
-    fix32<12>(0.649f)
+    0.949f
 };
 
 // libretro mGBA GBA shader color.
 const ColorProfile mGba01 =
 {
     {
-        { fix32<12>(0.84f), fix32<12>(0.18f), fix32<12>(0.f) },
-        { fix32<12>(0.09f), fix32<12>(0.67f), fix32<12>(0.26f) },
-        { fix32<12>(0.15f), fix32<12>(0.10f), fix32<12>(0.73f) }
+        { 0.84f, 0.18f, 0.f },
+        { 0.09f, 0.67f, 0.26f },
+        { 0.15f, 0.10f, 0.73f }
     },
-    fix32<12>(0.99f)
+    0.99f
 };
 
 constexpr const ColorProfile* colorProfileLut[] =
